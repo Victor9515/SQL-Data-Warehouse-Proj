a71d314c5096c808ad06af244a9fe6a7b7416bda
Victor9515 <victor.estrada363@gmail.com>
2026-01-19 13:49:22 -0800

    Create gold views for products, sales, and customers
    This script creates views in the gold layer of the data warehouse, including dimensions for products and customers, and a fact table for sales.

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,87 @@
+/*
+--======================================
+DDL Script: Create Gold Views
+--======================================
+Script Purpose:
+
+This script creates views in the gold layer in the data warehouse.
+The gold layer portrays a star schema, specifically representing
+the final dimension and fact tables.
+
+Each view performs transformations and combines data from the silver
+layer to product a clean, enrished, and business-ready data set.
+
+Usage:
+
+Views can be queried directly for analytics and reporting. 
+
+--======================================
+*/
+
+--======================================
+--CREATE Dimension: gold.dim_products
+--======================================
+CREATE VIEW gold.dim_products AS
+
+SELECT 
+ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key,
+pn.prd_id AS product_id,
+pn.prd_key AS product_number,
+pn.prd_nm AS product_name,
+pn.cat_id AS category_id,
+gc.cat AS category,
+gc.subcat AS subcategory,
+gc.maintenance,
+pn.prd_cost AS cost,
+pn.prd_line AS product_line,
+pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info AS pn
+LEFT JOIN silver.erp_px_cat_g1v2 AS gc
+ON pn.cat_id = gc.id
+WHERE prd_end_dt IS NULL 
+
+--======================================
+--CREATE Dimension: gold.fact_sale
+--======================================
+CREATE VIEW gold.fact_sale AS
+
+SELECT 
+      sd.sls_ord_num AS order_number,
+      pr.product_key,
+      cr.customer_key,
+      sd.sls_order_dt AS order_date,
+      sd.sls_ship_dt AS ship_date,
+      sd.sls_due_dt AS due_date,
+      sd.sls_sales AS sales_amount,
+      sd.sls_quantity AS quantity,
+      sd.sls_price AS price
+  FROM silver.crm_sales_details AS sd
+  LEFT JOIN gold.dim_products AS pr
+  ON sd.sls_prd_key = pr.product_number
+  LEFT JOIN gold.dim_customers AS cr
+  ON sd.sls_cust_id = cr.customer_id
+
+--======================================
+--CREATE Dimension: gold.dim_customers
+--======================================
+
+CREATE VIEW gold.dim_customers AS
+
+SELECT 
+ROW_NUMBER() OVER(ORDER BY cst_id) AS customer_key,
+ci.cst_id AS customer_id,
+ci.cst_key AS customer_number,
+ci.cst_firstname AS first_name,
+ci.cst_lastname AS last_name,
+la.cntry AS country,
+ci.cst_marital_status AS marital_status,
+CASE WHEN ci.cst_gndr != 'N/A' THEN ci.cst_gndr --CRM is master for gender info
+	ELSE COALESCE(ca.gen, 'N/A')
+END AS gender,
+ca.bdate AS birthdate,
+ci.cst_create_date AS create_date
+FROM silver.crm_cust_info AS ci
+LEFT JOIN silver.erp_cust_az12 AS ca
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 AS la
+ON ci.cst_key = la.cid
